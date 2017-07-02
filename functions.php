6161c661f231b1dfee9be4de0574cbba56ea7fc7
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-07-02 05:41:08 -0500

    I added normalize.css , modified the background color for do one experiment and i learned like use the function wp_register_style

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 <?php
 //Fuentes, tamaño de imagenes, agregar jquery, librerías de css se hace con funtions.php
 
-function lapizzeria_styles()
-{ // la funcion get_template_directory_uri() sirve para que se escriba la url
-	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0'); 
+function lapizzeria_styles(){ 	
+	//Lo registra en Wordpress para poderlo usar mas adelante
+	//En pocas palabras "Registra los estilos"
+	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0');
+	wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
+	
+	// la funcion get_template_directory_uri() sirve para que se escriba la url que va en las etiquetas "link"
+	//En pocas palabras "Llama a los estilos"
+	wp_enqueue_style('normalize');
+	wp_enqueue_style('style'); 
 }
 //la funcion wp_enqueue_scripts sirve para comunicarse con Wordpress
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
