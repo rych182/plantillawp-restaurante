465ed0acfb6d88e5b2bc2b3afc33b3d38711be0a
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-07-21 16:02:57 -0500

    El texto se adapta a las cajas de manera responsive

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,28 @@ $(document).ready(function() {
 	var breakpoint = 768;
 
 	$(window).resize(function() {
+		ajustarCajas();//para que la funcion se ejecute aquí
+		
 		if ($(document).width() >= breakpoint) {
 			$('nav.menu-sitio').show();
 		}else {
 			$('nav.menu-sitio').hide();
 		}
 	});
+
+	//Ajustar cajas según tamaño de imagen
+	ajustarCajas();
+		
 });
+
+function ajustarCajas() {
+		var imagenes = $('.imagen-caja');
+
+		if (imagenes.length > 0 ) {
+			var altura = imagenes[0].height;//con height podemos tomar la altura de las imagenes
+			var cajas = $('div.contenido-caja');
+			$(cajas).each(function(i, elemento){//i es el indice
+				$(elemento).css({'height' : altura + 'px'});
+			});
+		}
+	}
