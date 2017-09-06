f92cbafcc8d7bc4745865381ba89d7f386081222
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-09-06 02:53:23 -0500

    Hice publicaciones

--- single.php
+++ single.php
@@ -30,5 +30,20 @@ la funcion 'hace_posts():' hace eso. se encarga de revisar cuando el loop a fina
 	<div class="contenedor comentarios">
 		<?php comment_form(); ?> <!--Función que va a imprimir el formulario de los comentarios-->
 	</div>
+	<div class="contenedor">
+		<ul class="lista-comentarios">
+			<?php //get_comments nos regresa los comentarios, nos pasa un arreglo con distintos ajustes
+			//requiere 2 parametros, post_id y $post->ID
+				$comentarios = get_comments(array(
+					'post_id' => $post->ID,
+					'status' => 'approve'//Para que se vean solo los comentarios aprobados
+				));
+				wp_list_comments(array(
+					'per_page' => 10, //numero de comentarios por páginas
+					'reverse_top_level' => false //hace que los ultimos comentarios aparezcan hasta arriba
+					), $comentarios);//para que ponga los comentarios que solo estén aprobados
+			?>
+		</ul>
+	</div>
 <?php endwhile; ?>
 <?php get_footer(); ?>
