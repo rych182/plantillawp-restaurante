2e1b17f8470fabb382de101088b27c6193ef0b19
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-09-08 02:43:35 -0500

    Se creo una opción para actualización

--- inc/database.php
+++ inc/database.php
@@ -25,8 +25,31 @@ function lapizzeria_database(){
 
 	add_option('lapizzeria_version', $lapizzeria_dbversion);
 
+// ACTUALIZAR EN CASO DE SER NECESARIO
+
+$version_actual = get_option('lapizzeria_version');
+//Checa si es diferente, por si se hace cambios y así se crea la versión 0.2
+if ($lapizzeria_dbversion != $version_actual) {
+		$tabla = $wpdb->prefix . 'reservaciones';
+
+			$sql = "CREATE TABLE $tabla (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			nombre varchar(50) NOT NULL,
+			fecha datetime NOT NULL,
+			correo varchar(50) DEFAULT '' NOT NULL,
+			telefono varchar(10) NOT NULL,
+			mensaje longtext NOT NULL,
+			PRIMARY KEY (id)
+		) $charset_collate; ";
+		
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+		dbDelta($sql);
+
+		update_option('lapizzeria_dbversion', $lapizzeria_dbversion);
+	}
 
 }
 
-add_action('after_setup_theme', 'lapizzeria_database');
+
 ?>
