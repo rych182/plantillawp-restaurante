d24769e19a2f419286b4cd98e4267a9b44a7d81a
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-09-08 02:27:23 -0500

    creando codigo sql

--- inc/database.php
+++ inc/database.php
@@ -8,6 +8,24 @@ function lapizzeria_database(){
 	$tabla = $wpdb->prefix . 'reservaciones';
 
 	$charset_collate = $wpdb->get_charset_collate();	
+
+	$sql = "CREATE TABLE $tabla (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			nombre varchar(50) NOT NULL,
+			fecha datetime NOT NULL,
+			correo varchar(50) DEFAULT '' NOT NULL,
+			telefono varchar(10) NOT NULL,
+			mensaje longtext NOT NULL,
+			PRIMARY KEY (id)
+		) $charset_collate; ";
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+	dbDelta($sql);
+
+	add_option('lapizzeria_version', $lapizzeria_dbversion);
+
+
 }
 
 add_action('after_setup_theme', 'lapizzeria_database');
