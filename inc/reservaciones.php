0f8b3b3e0817d382fc32971b1578a5ae143a7a4f
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-09-08 21:53:40 -0500

    Insertando datos en el formulario

--- inc/reservaciones.php
+++ inc/reservaciones.php
@@ -1,6 +1,10 @@
 <?php
 
 function lapizzeria_guardar(){
+	//Global sirve para tener todas las opciones del framework
+	global $wpdb;
+	$tabla = $wpdb->prefix . "reservaciones";
+
 	if (isset($_POST['enviar']) && $_POST['oculto'] == "1" ):
 
 	$nombre = sanitize_text_field( $_POST['nombre'] );
@@ -9,7 +13,25 @@ function lapizzeria_guardar(){
 	$telefono = sanitize_text_field( $_POST['telefono'] );
 	$mensaje = sanitize_text_field( $_POST['mensaje'] );
 
-	endif; 
+	endif;
+
+	$datos = array(
+		'nombre' => $nombre,
+		'fecha' => $fecha,
+		'correo' => $correo,
+		'telefono' => $telefono,
+		'mensaje' => $mensaje,
+	); 
+
+	$formato = array(
+		'%s',
+		'%s',
+		'%s',
+		'%s',
+		'%s'
+		);
+
+	$wpdb->insert($tabla, $datos, $formato);
 }
 
 add_action('init', 'lapizzeria_guardar');
