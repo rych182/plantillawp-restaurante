a353a019d16c8da323c3f94f9b29db6a99222fd6
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-09-09 20:01:40 -0500

    Mostrando e imprimiendo insercciones de nuestras tablas de reservaciones en el backoffice

--- inc/opciones.php
+++ inc/opciones.php
@@ -17,6 +17,42 @@ function lapizzeria_opciones(){
 
 function lapizzeria_reservaciones (){
 	?>
+	<div class="wrap">
+		<h1>Informacion de clientes</h1>
 
-	<?php
+		<table class="wp-list-table widefat striped">
+			<thead>
+				<tr>
+					<th class="manage-column">ID</th>
+					<th class="manage-column">Nombre</th>
+					<th class="manage-column">Fecha</th>
+					<th class="manage-column">Correo</th>
+					<th class="manage-column">Teléfono</th>
+					<th class="manage-column">Mensaje</th>
+				</tr>
+			</thead>
+			<tbody>
+				<!--Así es como vamos a imprimir esos registros de nuestra tabla-->
+				<?php global $wpdb; 
+
+				$reservaciones = $wpdb->prefix . 'reservaciones';
+				//Imprime todos los registros de la tabla a la que quiero hacer el query(consulta)
+				//Gracias al $wpdb->prefix podemos acceder a la tabla sin importar si el usuario cambio el prefijo de la tabla o no
+				$registros = $wpdb->get_results(" SELECT * FROM $reservaciones ", ARRAY_A);
+				foreach ($registros as $registro) {?>
+
+				<tr>
+					<td><?php echo $registro['id']; ?></td>
+					<td><?php echo $registro['nombre']; ?></td>
+					<td><?php echo $registro['fecha']; ?></td>
+					<td><?php echo $registro['correo']; ?></td>
+					<td><?php echo $registro['telefono']; ?></td>
+					<td><?php echo $registro['mensaje']; ?></td>
+				</tr>
+			
+			<?php }?>
+			</tbody>
+		</table>
+	</div>
+	<?php 
 }
